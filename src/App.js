940da31aa3b6bf2172bfc838677dda54effb669d
Lakshmi5devi <164911607+Lakshmi5devi@users.noreply.github.com>
2024-06-10 08:29:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}></Route>
+          <Route path='/men' element={<ShopCategory banner={men_banner} category="men"/>}></Route>
+          <Route path='/women' element={<ShopCategory banner={women_banner} category="women"/>}></Route>
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid"/>}></Route>
+          <Route path="/product" element={<Product/>}>
+              <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+      
+        
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>An e-commerce website ia an online platform that facilitate buying and selling of products or services over the internet serves as a avirtual marketplace where business people and individual showcase thei products, interact with customers and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenient accessibility, and the global reach they offer.</p>
+                <p>E-commerce websites typically display products or services ad detailed descriptions, images, prices and any availabie varities. Each product usually has its own dedication with relevant information.</p>
+            </div>
+
+        </div>
+    )
+}
+export default DescriptionBox;
